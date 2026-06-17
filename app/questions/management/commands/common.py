66120dfbff823007af104f55ec16c6bcf503fb59
Kajta <kajtakamil1@gmail.com>
2026-06-17 23:44:42 +0200

    feat: adding video format converter to parser

--- app/questions/management/commands/common.py
+++ app/questions/management/commands/common.py
@@ -1,5 +1,8 @@
+import os
 import shutil
 from pathlib import Path
+
+from moviepy import VideoFileClip
 
 from questions.models import Question
 
@@ -15,6 +18,20 @@ def flatten_file_structure(directory):
         if item.is_dir():
             shutil.rmtree(str(item))
 
+def convert_wmv_to_mp4(file_path):
+    full_path = str(Path(file_path).resolve())
+    output_path = full_path.replace('.wmv', '.mp4')
+    clip = VideoFileClip(str(file_path))
+    clip.write_videofile(str(output_path), codec="libx264")
+    clip.close()
+    os.remove(full_path)
+
+
+def directory_convert_wmv_to_mp4(directory):
+    path = Path(directory)
+    for file in path.rglob('*.wmv'):
+        if file.is_file():
+            convert_wmv_to_mp4(file)
 
 def print_missing_files():
     questions = Question.objects.all()
